05ee7ad215faf0a2033b277f29bfd98254a1a6a4
Alan <alan.melgar.f@gmail.com>
2023-08-27 15:18:28 -0400

    routes(rutas del servidor)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import express from 'express'
-import {pool} from './db.js';
+import employeesRoutes  from './routes/employees.routes.js';
+import indexRoutes from './routes/index.routes.js';
 
 const app = express()
 
-app.get('/ping', async (req,res)=>{
-    const [result] = await pool.query('SELECT "PONG" AS result') 
-    res.json(result[0])
-});
-
-app.get('/employees', (req,res)=>res.send('Obteniendo empleados'));
-
-app.post('/employees', (req,res)=>res.send('creando empleados'));
-
-app.put('/employees', (req,res)=>res.send('actualizando empleados'));
-
-app.delete('/employees', (req,res)=>res.send('eliminando empleados'));
-
-
+app.use(indexRoutes)
+app.use(employeesRoutes)
 
 app.listen(3000)
 console.log('Server on port 3000')

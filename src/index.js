eafd3c2601686497beee89034e3be1e6f1c83eb0
Alan <alan.melgar.f@gmail.com>
2023-08-27 14:04:46 -0400

    End Points (rutas-servidor)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import expres from 'express'
 
 const app = expres()
 
+app.get('/employees', (req,res)=>res.send('Obteniendo empleados'))
+
+app.post('/employees', (req,res)=>res.send('creando empleados'))
+
+app.put('/employees', (req,res)=>res.send('actualizando empleados'))
+
+app.delete('/employees', (req,res)=>res.send('eliminando empleados'))
+
+
+
 app.listen(3000)
 console.log('Server on port 3000')

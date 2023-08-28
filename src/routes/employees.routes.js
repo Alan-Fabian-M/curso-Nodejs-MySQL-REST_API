b6a839d8dfc1e39b8915cb175ffab0516d61e876
Alan <alan.melgar.f@gmail.com>
2023-08-27 23:31:12 -0400

    GET Employee by id

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import {getEmployees, createEmployees, updateEmployees, deleteEmployees} from "../controllers/employees.controllers.js";
+import {getEmployees, createEmployees, updateEmployees, deleteEmployees, getEmployee} from "../controllers/employees.controllers.js";
 
 const router = Router();
 
 router.get('/employees', getEmployees );
+
+router.get('/employee/:id', getEmployee );
 
 router.post('/employees', createEmployees);
 

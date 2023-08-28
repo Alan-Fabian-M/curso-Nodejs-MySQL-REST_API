b049b0c88b08d9ecd1a9a671d3150819b372444c
Alan <alan.melgar.f@gmail.com>
2023-08-28 00:51:53 -0400

    mensaje 'not found'

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ import indexRoutes from './routes/index.routes.js';
 const app = express()
 
 app.use(express.json())
+
 app.use(indexRoutes)
 app.use('/api/',employeesRoutes)
+
+app.use((req,res,next)=>{
+    res.status(404).json({
+        message: 'endpoint not found'
+    })
+})
 
 app.listen(3000)
 console.log('Server on port 3000')
